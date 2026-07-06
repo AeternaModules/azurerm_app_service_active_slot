08d216ff01250c33a57f48458a8cf2d829106268
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "app_service_active_slots" {
   description = "All app_service_active_slot resources"
   value       = azurerm_app_service_active_slot.app_service_active_slots
 }
+output "app_service_active_slots_app_service_name" {
+  description = "List of app_service_name values across all app_service_active_slots"
+  value       = [for k, v in azurerm_app_service_active_slot.app_service_active_slots : v.app_service_name]
+}
+output "app_service_active_slots_app_service_slot_name" {
+  description = "List of app_service_slot_name values across all app_service_active_slots"
+  value       = [for k, v in azurerm_app_service_active_slot.app_service_active_slots : v.app_service_slot_name]
+}
+output "app_service_active_slots_resource_group_name" {
+  description = "List of resource_group_name values across all app_service_active_slots"
+  value       = [for k, v in azurerm_app_service_active_slot.app_service_active_slots : v.resource_group_name]
+}
 
