454ab5f86126dc2b2c15ed5dc14a3ad4dfd065b1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "app_service_active_slots" {
-  description = "All app_service_active_slot resources"
-  value       = azurerm_app_service_active_slot.app_service_active_slots
-}
 output "app_service_active_slots_app_service_name" {
-  description = "List of app_service_name values across all app_service_active_slots"
-  value       = [for k, v in azurerm_app_service_active_slot.app_service_active_slots : v.app_service_name]
+  description = "Map of app_service_name values across all app_service_active_slots, keyed the same as var.app_service_active_slots"
+  value       = { for k, v in azurerm_app_service_active_slot.app_service_active_slots : k => v.app_service_name }
 }
 output "app_service_active_slots_app_service_slot_name" {
-  description = "List of app_service_slot_name values across all app_service_active_slots"
-  value       = [for k, v in azurerm_app_service_active_slot.app_service_active_slots : v.app_service_slot_name]
+  description = "Map of app_service_slot_name values across all app_service_active_slots, keyed the same as var.app_service_active_slots"
+  value       = { for k, v in azurerm_app_service_active_slot.app_service_active_slots : k => v.app_service_slot_name }
 }
 output "app_service_active_slots_resource_group_name" {
-  description = "List of resource_group_name values across all app_service_active_slots"
-  value       = [for k, v in azurerm_app_service_active_slot.app_service_active_slots : v.resource_group_name]
+  description = "Map of resource_group_name values across all app_service_active_slots, keyed the same as var.app_service_active_slots"
+  value       = { for k, v in azurerm_app_service_active_slot.app_service_active_slots : k => v.resource_group_name }
 }
 
